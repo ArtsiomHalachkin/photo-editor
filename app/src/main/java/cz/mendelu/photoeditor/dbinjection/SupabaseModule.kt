8d060b52287536455848ba25237b4fe779d3d531
ArtsiomHalachkin <art.nmk129@gmail.com>
2025-06-08 22:41:06 +0200

    delete key and url

--- app/src/main/java/cz/mendelu/photoeditor/dbinjection/SupabaseModule.kt
+++ app/src/main/java/cz/mendelu/photoeditor/dbinjection/SupabaseModule.kt
@@ -17,8 +17,8 @@ object SupabaseModule {
     @Singleton
     fun provideSupabaseClient(): SupabaseClient {
         return createSupabaseClient(
-            supabaseUrl = "https://ittpnpvhherohscrkgjd.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0dHBucHZoaGVyb2hzY3JrZ2pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NjEyOTEsImV4cCI6MjA2MzIzNzI5MX0.62DaI3LWvLbzGOC9LBurcI9V8NTXlzTxutS8va6rugI"
+            supabaseUrl = "your-supabase-url",
+            supabaseKey = "your-supabase-key"
         ){
             install(Storage)
         }
